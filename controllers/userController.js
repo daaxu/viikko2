8dc3d5ff742ae9a4ce91aa79cf394f4068af096a
Daaxu <aleksi.knuuttila@metropolia.fi>
2020-10-28 11:35:57 +0200

    some message

---
+++ controllers/userController.js
@@ -0,0 +1,23 @@
+// Controller
+'use strict';
+const userModel = require('../models/userModel');
+
+
+const users = userModel.users;
+
+const user_list_get = (req, res) => {
+  const newUsers = users.map(usr => delete usr.password);
+  res.json(newUsers);
+};
+
+const user_get = (req, res) => {
+  const id = req.params.id;
+  const user = users.filter(usr => usr.id === id).pop();
+  delete user.password;
+  res.json(user);
+};
+
+module.exports = {
+  user_list_get,
+  user_get,
+};
