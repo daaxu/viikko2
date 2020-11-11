d046ae5a9368ad58efb5ffd3e3749d0d90737b87
Daaxu <aleksi.knuuttila@metropolia.fi>
2020-11-11 13:20:23 +0200

    some message

--- models/userModel.js
+++ models/userModel.js
@@ -1,43 +1,60 @@
-// ./models/userModel.js
 'use strict';
 const pool = require('../database/db');
-const { param } = require('../routes/userRoute');
 const promisePool = pool.promise();
 
 const getAllUsers = async () => {
   try {
     // TODO: do the LEFT (or INNER) JOIN to get owner name too.
     const [rows] = await promisePool.query('SELECT * FROM wop_user');
+    console.log('rows', rows);
     return rows;
   } catch (e) {
     console.log('userModel error', e.message);
-  }   
+    return {error: 'DB Error'};
+  }
 };
 
 const getUser = async (id) => {
   try {
-    const [rows] = await promisePool.execute('SELECT * FROM wop_user WHERE user_id = ?',[id]);
+    const [rows] = await promisePool.execute('SELECT * FROM wop_user WHERE user_id = ?',
+        [id]);
+    console.log('rows', rows);
     return rows;
   } catch (e) {
     console.log('userModel error', e.message);
-    return {error: 'DB error'};
-  } 
-};
+    return {error: 'DB Error'};
+  }
+}
 
-
-
-const adduser = async (id) => {
+const addUser = async (params) => {
   try {
-    const [rows] = await promisePool.execute('INSERT INTO wop_user (name, email, password) VALUES (?,?,?)', params);
+    const [rows] = await promisePool.execute(
+        'INSERT INTO wop_user (name, email, password) VALUES (?,?,?)',
+        params
+    );
+    console.log('rows', rows);
     return rows;
   } catch (e) {
     console.log('userModel error', e.message);
-    return {error: 'DB error'};
-  } 
-};
+    return {error: 'DB Error'};
+  }
+}
 
+const getUserLogin = async (params) => {
+  try {
+    console.log('getUserLogin', params);
+    const [rows] = await promisePool.execute(
+        'SELECT * FROM wop_user WHERE email = ?;',
+        params);
+    return rows;
+  } catch (e) {
+    console.log('error', e.message);
+  }
+};
 
 module.exports = {
   getAllUsers,
   getUser,
+  addUser,
+  getUserLogin,
 };
